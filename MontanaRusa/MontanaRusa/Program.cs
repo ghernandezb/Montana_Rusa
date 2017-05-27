b005348be0801b767a7c1787091bdbb225bdf948
Gabriel Hernandez Bermudez <gabriel-hernandez@live.com>
2017-05-27 15:57:08 -0600

    Primera funcionalidad

--- MontanaRusa/MontanaRusa/Program.cs
+++ MontanaRusa/MontanaRusa/Program.cs
@@ -17,13 +17,12 @@ namespace MontanaRusa
         //----- MENU -----//
         public static void Menu()
         {
-            MontanaRusa montanaRusa = new MontanaRusa(2, 3, 1.6f, 3, 5);
+            MontanaRusa montanaRusa = new MontanaRusa(2, 3, 1.5f, 3, 5);
 
             bool opcionMenu = true;
-            int numeroMenu = 0;
-            int valorInicialMenu = 1;
-            int valorFinalMenu = 4;
-
+            int numeroMenu = -1;
+            int valorInicialMenu = 0;
+            int valorFinalMenu = 6;
 
             Console.WriteLine("Hola!");
             Console.WriteLine("");
@@ -84,10 +83,22 @@ namespace MontanaRusa
                                     i--;
                                 }
                             }
-                            Console.WriteLine("Han ingresado "+ numeroIngresados + " nuevos pasajeros.");
+
+                            Console.WriteLine("Han ingresado " + numeroIngresados + " nuevos pasajeros.");
                             Console.WriteLine("");
-                            Console.WriteLine("");
-                            Console.WriteLine("");
+
+                            if (montanaRusa.ObtenerCampoDisponible() == null)
+                            {
+                                montanaRusa.FechaEspera = null;
+                                Console.WriteLine("Todos los carritos estan llenos, listo para salir.");
+                                Console.WriteLine("");
+                            }
+                            else if (numeroIngresados > 0 && montanaRusa.FechaEspera == null)
+                            {
+                                montanaRusa.FechaEspera = DateTime.Now;
+                                Console.WriteLine("Carritos aun no llenos, esperando mas pasajeros.");
+                                Console.WriteLine("");
+                            }
                         }
                         else
                         {
@@ -104,6 +115,39 @@ namespace MontanaRusa
 
                     case 3:
                         // Refrescar lista de espera
+                        Random ran = new Random();
+                        int numeroRandom = ran.Next(0, 15);
+
+                        for (int i = 1; i<= numeroRandom; i++)
+                        {
+                            string nombre = Listas.nombres[ran.Next(0, Listas.nombres.Count - 1)] 
+                                          + " " 
+                                          + Listas.apellidos[ran.Next(0, Listas.apellidos.Count - 1)] 
+                                          + " " 
+                                          + Listas.apellidos[ran.Next(0, Listas.apellidos.Count - 1)];
+                            float altura = (float)Math.Round(NextDouble(1, 2), 2);
+                            PersonaEstadosAnimos animoActual;
+                            int estado = ran.Next(0, 100);
+
+                            if (estado <= 33)
+                            {
+                                animoActual = PersonaEstadosAnimos.Normal;
+                            }
+                            else if (estado <= 66)
+                            {
+                                animoActual = PersonaEstadosAnimos.Asustado;
+                            }
+                            else
+                            {
+                                animoActual = PersonaEstadosAnimos.Enojado;
+                            }
+                            montanaRusa.ListaEspera.Add(new Persona(nombre, altura, animoActual));
+                            Console.WriteLine(nombre + " ha agregado a la fila.");
+                            Console.WriteLine("");
+                        }
+
+                        Console.WriteLine("Han ingresado " + numeroRandom + " a la fila.");
+                        Console.WriteLine("");
 
                         break;
 
@@ -150,6 +194,15 @@ namespace MontanaRusa
             }
             return opcion;
         }
+        public static double NextDouble(double min, double max)
+        {
+            return new Random().NextDouble() * (max - min) + min;
+        }
+    }
 
+    class Listas
+    {
+        public static readonly List<string> nombres = new List<string>(new string[] { "Emma", "Liam", "Olivia", "Noah", "Ava", "Mason", "Sophia", "Lucas", "Isabella", "Oliver", "Mia", "Ethan", "Charlotte", "Elijah", "Amelia", "Aiden", "Harper", "Logan", "Aria", "James", "Abigail", "Benjamin", "Ella", "Jackson", "Evelyn", "Jacob", "Avery", "Carter", "Emily", "Sebastian", "Madison", "Alexander", "Scarlett", "Michael", "Sofia", "Matthew", "Lily", "Jayden", "Mila", "Jack", "Riley", "Luke", "Layla", "Wyatt", "Chloe", "Daniel", "Ellie", "Gabriel", "Grace", "William", "Zoey", "Grayson", "Penelope", "Henry", "Aubrey", "Owen", "Elizabeth", "Levi", "Victoria", "Jaxon", "Hannah", "Lincoln", "Nora", "Adam", "Stella", "David", "Addison", "Isaiah", "Luna", "Joseph", "Natalie", "Julian", "Paisley", "Josiah", "Skylar", "Ryan", "Savannah", "Samuel", "Maya", "Eli", "Camila", "Dylan", "Hazel", "Nathan", "Lillian", "Joshua", "Lucy", "Isaac", "Bella", "John", "Brooklyn", "Caleb", "Audrey", "Andrew", "Aaliyah", "Hunter", "Leah", "Leo", "Zoe", "Muhammad" });
+        public static readonly List<string> apellidos = new List<string>(new string[] { "Smith", "Johnson", "Williams", "Brown", "Jones", "Miller", "Davis", "Garcia", "Rodriguez", "Wilson", "Martinez", "Anderson", "Taylor", "Thomas", "Hernandez", "Moore", "Martin", "Jackson", "Thompson", "White", "Lopez", "Lee", "Gonzalez", "Harris", "Clark", "Lewis", "Robinson", "Walker", "Perez", "Hall", "Young", "Allen", "Sanchez", "Wright", "King", "Scott", "Green", "Baker", "Adams", "Nelson", "Hill", "Ramirez", "Campbell", "Mitchell", "Roberts", "Carter", "Phillips", "Evans", "Turner", "Torres", "Parker", "Collins", "Edwards", "Stewart", "Flores", "Morris", "Nguyen", "Murphy", "Rivera", "Cook", "Rogers", "Morgan", "Peterson", "Cooper", "Reed", "Bailey", "Bell", "Gomez", "Kelly", "Howard", "Ward", "Cox", "Diaz", "Richardson", "Wood", "Watson", "Brooks", "Bennett", "Gray", "James", "Reyes", "Cruz", "Hughes", "Price", "Myers", "Long", "Foster", "Sanders", "Ross", "Morales", "Powell", "Sullivan", "Russell", "Ortiz", "Jenkins", "Gutierrez", "Perry", "Butler", "Barnes", "Fisher" });
     }
 }
